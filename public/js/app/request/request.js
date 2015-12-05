f3ddd2fa98ccdd60cf12111927b4784f6d32db3f
Przech <arthur.przech@gmail.com>
2015-12-05 03:25:16 -0500

    added start of page for making a request

---
+++ public/js/app/request/request.js
@@ -0,0 +1,41 @@
+angular.module('MyApp.request', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/request/:tutorId', {
+            templateUrl: 'js/app/request/request.html',
+            controller: 'RequestController'
+        });
+}])
+
+.controller("RequestController", ['$scope','$http','$rootScope', '$routeParams', function($scope, $http, $rootScope,$routeParams) {
+
+	$http.get("/api/tutors/" + $routeParams.tutorId).then(function sucessCallback (res){
+
+		console.log(res.data.data);
+	});
+
+
+/*
+	if ($rootScope.actor){
+		if ($rootScope.actor.userType === 'Students'){
+			$scope.userType = $rootScope.actor.userType;
+			$http.get("/api/students/" +  $rootScope.actor._id + "/requests").then(
+	            function successCallback(res){
+	                    $scope.requests = res.data.data;
+	                    $scope.requests.forEach(function(req){
+	                        req.active = false;
+	                    });
+
+	                });
+
+		} else {
+			window.location.href = '/#/';
+		}
+
+	} else {
+		window.location.href = '/#/';
+	}
+*/
+
+}]);
