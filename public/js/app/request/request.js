3b536c22ab41a8bb09ba0e585e8051e993cb108b
Przech <arthur.przech@gmail.com>
2015-12-05 17:22:23 -0500

    cleaned up request.js

--- public/js/app/request/request.js
+++ public/js/app/request/request.js
@@ -10,16 +10,11 @@ angular.module('MyApp.request', ['ngRoute'])
 
 .controller("RequestController", ['$scope','$http','$rootScope', '$routeParams', function($scope, $http, $rootScope,$routeParams) {
 
-	$http.get("/api/tutors/" + $routeParams.tutorId).then(function sucessCallback (res){
-
-		console.log(res.data.data);
-	});
-
 	$scope.topics = [];
 	$scope.topicName = "";
 	$scope.requestMessage = '';
 
-	
+	//Getting current User.
 	$http.get('/api/actor').then(
         function successCallback(res){
             $rootScope.actor = res.data.data;
@@ -33,31 +28,27 @@ angular.module('MyApp.request', ['ngRoute'])
 	                console.log("Error getting tutor");
 	                $rootScope.displayAlert('error',res.data.message);
 		        });
-	
 			} else {
 				window.location.href = '/#/';
-			}
-		},  function errorCallback(res){
+			}},  
+
+			function errorCallback(res){
 	                //trigger error message here.
 	                console.log("Error getting actor");
 	                $rootScope.displayAlert('error',res.data.message);
 		    });
 
-
-
+	// Send a request to server under topic name and with a non empty message.
 	$scope.sendRequest =  function(tpcName, reqMessage){
 		if (reqMessage === ""){
-
             console.log("Attempted empty request.");
             $rootScope.displayAlert('error', "Must provide message in request.");  
 		} else {
 			$http.post('/api/students/'+ $rootScope.actor._id + '/requests', 
 				{tutorId : $routeParams.tutorId, topicName: tpcName, message: reqMessage}).then(
 					function sucessCallback (res){
-						window.location.href = '/#/';
-											
+						window.location.href = '/#/';		
 					}, 				
-
 					 function errorCallback(res){
 		                //trigger error message here.
 		                console.log("Error sending request");
@@ -66,26 +57,5 @@ angular.module('MyApp.request', ['ngRoute'])
 		}
 
 	};
-/*
-	if ($rootScope.actor){
-		if ($rootScope.actor.userType === 'Students'){
-			$scope.userType = $rootScope.actor.userType;
-			$http.get("/api/students/" +  $rootScope.actor._id + "/requests").then(
-	            function successCallback(res){
-	                    $scope.requests = res.data.data;
-	                    $scope.requests.forEach(function(req){
-	                        req.active = false;
-	                    });
-
-	                });
-
-		} else {
-			window.location.href = '/#/';
-		}
-
-	} else {
-		window.location.href = '/#/';
-	}
-*/
 
 }]);
