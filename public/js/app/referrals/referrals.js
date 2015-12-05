12c5740ddcd8b68abbc1d0c47cd9bc36adf7b6cd
Przech <arthur.przech@gmail.com>
2015-12-05 02:00:40 -0500

    Added referrals page

---
+++ public/js/app/referrals/referrals.js
@@ -0,0 +1,39 @@
+angular.module('MyApp.referrals', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/referrals', {
+            templateUrl: 'js/app/referrals/referrals.html',
+            controller: 'ReferralsController'
+        });
+}])
+.controller("ReferralsController", ['$scope','$http','$rootScope', function($scope, $http, $rootScope) {
+
+
+	if ($rootScope.actor){
+		if ($rootScope.actor.userType === 'Students'){
+			$scope.userType = $rootScope.actor.userType;
+			$http.get("/api/students/" +  $rootScope.actor._id + "/referrals").then(
+	            function successCallback(res){
+	            		
+	                    $scope.referrals = res.data.data;
+	                   	$scope.referrals.forEach(function(ref){
+                            $http.put("/api/students/"+  $rootScope.actor._id + "/referrals/" + ref._id, {isRead: true}).then(
+                            	function successCallback(res){
+                            		ref.isRead = true;
+                            	});
+                        });
+                        
+	                    console.log($scope.referrals);
+
+	                });
+
+		} else {
+			window.location.href = '/#/';
+		}
+
+	} else {
+		window.location.href = '/#/';
+	}
+
+}]);
