ef5c3b385345de92d84c385859f03243971a568c
Parker Aldric Mar <parkeraldricmar@gmail.com>
2015-12-06 22:58:34 -0500

    server.js modified for local run

--- server.js
+++ server.js
@@ -17,7 +17,13 @@ var uuid = require('uuid');
 app.mongoose = mongoose;
 
 //To connect to MongoDB's database
-mongoose.connect('mongodb://heroku_9g2w3hbk:beoftgpg361fiht6a0tsnqb7qu@ds041924.mongolab.com:41924/heroku_9g2w3hbk');
+//mongoose.connect('mongodb://heroku_9g2w3hbk:beoftgpg361fiht6a0tsnqb7qu@ds041924.mongolab.com:41924/heroku_9g2w3hbk');
+
+//To connect locally
+mongoose.connect('mongodb://localhost/bazahmed', {
+    user: '',
+    pass: ''
+});
 
 //check the status of this connection
 var db = mongoose.connection;
@@ -50,8 +56,15 @@ app.activityLogger = new ActivityLogger(app);
 //Setup routes
 require('./routes')(app);
 
-app.set('port', (process.env.PORT || 3000));
-app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
-});
+//To connect to heroku online app
+//app.set('port', (process.env.PORT || 3000));
+//app.listen(app.get('port'), function() {
+//    console.log('Node app is running on port', app.get('port'));
+//});
+
+//To connect locally
+app.listen(3000,'0.0.0.0');
+
+console.log('Server running at http://127.0.0.1:3000/');
+
 module.exports = app;
